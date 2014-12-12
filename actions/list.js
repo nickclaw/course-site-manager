de407b66491a38f0e1ec3e1ddef177ff4b0a5fe5
Nicholas Clawson <nickclaw@gmail.com>
2014-12-11 17:09:41 -0800

    Add optional key to list command.

--- actions/list.js
+++ actions/list.js
@@ -2,15 +2,28 @@ var _ = require('lodash');
 
 module.exports = function(program, config) {
 
-    var command = program.command('list')
+    var command = program.command('list [key]')
     .description('Lists all installed instances.')
     .option('-v, --verbose', 'Show more data.')
-    .action(function() {
+    .action(function(key) {
 
         var instances = config.instances;
 
         if (!_.size(instances)) {
             console.log('There are no instances installed.');
+            return;
+        }
+
+        if (key) {
+            if (!instances[key]) {
+                console.log('No instance installed named: ' + key);
+                return;
+            } else {
+                var instance = instances[key];
+                instances = {};
+                instances[key] = instance;
+                command.verbose = true;
+            }
         }
 
         _.each(instances, function(instance, key) {
@@ -19,7 +32,7 @@ module.exports = function(program, config) {
                 console.log('  path:    ', instance.path);
                 console.log('  webRoot: ', instance.webRoot || instance.path + '/web');
                 console.log('  created: ', instance.created);
-                console.log('  updated: ', instances.updated);
+                console.log('  updated: ', instance.updated);
                 console.log();
             }
         });
